f508a5fbe6bbd5f101019a03ab8ba3bd495d0eae
formulariologin <joshuagg20@gmail.com>
2023-10-02 18:42:27 -0600

    second commit

--- apirest/apirest/src/main/java/com/ferremax/apirest/models/entity/Cliente.java
+++ apirest/apirest/src/main/java/com/ferremax/apirest/models/entity/Cliente.java
@@ -4,72 +4,40 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
+@Getter//sirve para generar los getter
+@Setter//sirve para generar los setter
+                                         //@FieldDefaults(level = AccessLevel.PACKAGE)//con esta notación evitamos definir los atrivutos ya sea como privado o publico
 @Table(name = "Clientes")
 public class Cliente implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+     private Long id;
     @NotEmpty(message="No puede estar vacio")
     @Size(min=4, max = 15,message = "El tamaño tiene que estar entre 4 y 15 caracteres")
     @Column(nullable = false)
-    private String nombre;
+     private String nombre;
     @NotEmpty(message = "No puede estar vacio")
     private String apellido;
     @NotEmpty(message = "No puede estar vacio")
     @Email(message = "No es una direccion de correo valida")
     @Column(nullable = false, unique = true)
-    private String email;
+     private String email;
     @Column(name = "create_at")
     @Temporal(TemporalType.DATE)
-    private Date creteAt;
+     private Date creteAt;
     @PrePersist
     public void prePersist(){
         creteAt= new Date();
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getCreteAt() {
-        return creteAt;
-    }
-
-    public void setCreteAt(Date creteAt) {
-        this.creteAt = creteAt;
-    }
     private static final long serialVersionUID=1L;
 }
